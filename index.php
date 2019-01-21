03522d81905a33c3701bd162505a317b30ed1f93
Manobala S <manobala.s@hotmail.com>
2019-01-21 17:57:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,23 @@
 <?php
-echo "Mano";
+function get_server_memory_usage(){
+	
+	$free = shell_exec('free');
+	$free = (string)trim($free);
+	$free_arr = explode("\n", $free);
+	$mem = explode(" ", $free_arr[1]);
+	$mem = array_filter($mem);
+	$mem = array_merge($mem);
+	$memory_usage = $mem[2]/$mem[1]*100;
+
+	return $memory_usage;
+}
+
+function get_server_cpu_usage(){
+
+	$load = sys_getloadavg();
+	return $load[0];
+
+}
+
+<p><span class="description">Server Memory Usage:</span> <span class="result">= get_server_memory_usage() ?&gt;%</span></p>
+<p><span class="description">Server CPU Usage: </span> <span class="result">= get_server_cpu_usage() ?&gt;%</span></p>
